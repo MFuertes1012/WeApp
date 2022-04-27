9c9c1fb8e8ace05ad5c470941ffa849d7cc64179
Margie Fuertes <85895396+MFuertes1012@users.noreply.github.com>
2022-04-27 11:08:10 -0600

    Add files via upload

---
+++ WeatherConcurrencyApp/WeatherPanel.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace WeatherConcurrencyApp
+{
+    partial class WeatherPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblTemperature = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblWeather = new System.Windows.Forms.Label();
+            this.flpContent = new System.Windows.Forms.FlowLayoutPanel();
+            this.picIcon = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCity.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(4, 0);
+            this.lblCity.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(417, 72);
+            this.lblCity.TabIndex = 0;
+            this.lblCity.Text = "City";
+            this.lblCity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTemperature
+            // 
+            this.lblTemperature.AutoSize = true;
+            this.lblTemperature.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTemperature.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemperature.Location = new System.Drawing.Point(4, 72);
+            this.lblTemperature.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTemperature.Name = "lblTemperature";
+            this.lblTemperature.Size = new System.Drawing.Size(417, 72);
+            this.lblTemperature.TabIndex = 1;
+            this.lblTemperature.Text = "30";
+            this.lblTemperature.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 425F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.Controls.Add(this.lblTemperature, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblCity, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblWeather, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(425, 170);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // lblWeather
+            // 
+            this.lblWeather.AutoSize = true;
+            this.lblWeather.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblWeather.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWeather.Location = new System.Drawing.Point(4, 144);
+            this.lblWeather.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblWeather.Name = "lblWeather";
+            this.lblWeather.Size = new System.Drawing.Size(417, 26);
+            this.lblWeather.TabIndex = 2;
+            this.lblWeather.Text = "lblWeather";
+            this.lblWeather.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // flpContent
+            // 
+            this.flpContent.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flpContent.Location = new System.Drawing.Point(0, 242);
+            this.flpContent.Margin = new System.Windows.Forms.Padding(4);
+            this.flpContent.Name = "flpContent";
+            this.flpContent.Size = new System.Drawing.Size(425, 436);
+            this.flpContent.TabIndex = 3;
+            // 
+            // picIcon
+            // 
+            this.picIcon.Location = new System.Drawing.Point(0, 173);
+            this.picIcon.Name = "picIcon";
+            this.picIcon.Size = new System.Drawing.Size(104, 68);
+            this.picIcon.TabIndex = 4;
+            this.picIcon.TabStop = false;
+            // 
+            // WeatherPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.picIcon);
+            this.Controls.Add(this.flpContent);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "WeatherPanel";
+            this.Size = new System.Drawing.Size(425, 678);
+            this.Load += new System.EventHandler(this.WeatherPanel_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flpContent;
+        public System.Windows.Forms.Label lblCity;
+        public System.Windows.Forms.Label lblTemperature;
+        public System.Windows.Forms.Label lblWeather;
+        public System.Windows.Forms.PictureBox picIcon;
+    }
+}
---
+++ WeatherConcurrencyApp/WeatherPanel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeatherConcurrencyApp.Infrastructure.OpenWeatherClient;
+using WeatherConcurrentApp.Domain.Entities;
+
+namespace WeatherConcurrencyApp
+{
+    public partial class WeatherPanel : UserControl
+    {
+        public HttpOpenWeatherClient httpOpenWeatherClient;
+        public OpenWeather openWeather;
+        public WeatherPanel()
+        {
+            httpOpenWeatherClient = new HttpOpenWeatherClient();
+            InitializeComponent();
+        }
+
+        private void WeatherPanel_Load(object sender, EventArgs e)
+        {
+            Task.Run(Request).Wait();
+            DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).ToLocalTime();
+            DateTime day1 = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).ToLocalTime();
+
+            DetailsWeather detailsWeather = new DetailsWeather();
+            detailsWeather.lblDetail.Text = "Details";
+            detailsWeather.lblDetailValue.Text = openWeather.Weather[0].Description;
+            flpContent.Controls.Add(detailsWeather);
+
+            DetailsWeather detailsWeather1 = new DetailsWeather();
+            detailsWeather1.lblDetail.Text = "amanecer";
+            day = day.AddSeconds(openWeather.Sys.Sunrise).ToLocalTime();
+            detailsWeather1.lblDetailValue.Text = day.ToShortTimeString();
+            flpContent.Controls.Add(detailsWeather1);
+
+            DetailsWeather detailsWeather2 = new DetailsWeather();
+            detailsWeather2.lblDetail.Text = "atardecer";
+            day1 = day1.AddSeconds(openWeather.Sys.Sunset).ToLocalTime();
+            detailsWeather2.lblDetailValue.Text = day1.ToShortTimeString();
+            flpContent.Controls.Add(detailsWeather2);
+
+            DetailsWeather detailsWeather3 = new DetailsWeather();
+            detailsWeather3.lblDetail.Text = "Wind Speed";
+            detailsWeather3.lblDetailValue.Text = openWeather.Wind.Speed.ToString() + " Km/h";
+            flpContent.Controls.Add(detailsWeather3);
+
+            DetailsWeather detailsWeather4 = new DetailsWeather();
+            detailsWeather4.lblDetail.Text = "Pressure";
+            detailsWeather4.lblDetailValue.Text = openWeather.Main.Pressure.ToString();
+            flpContent.Controls.Add(detailsWeather4);
+
+            DetailsWeather detailsWeather5 = new DetailsWeather();
+            detailsWeather5.lblDetail.Text = "Humidity";
+            detailsWeather5.lblDetailValue.Text = openWeather.Main.Humidity.ToString() + "%";
+            flpContent.Controls.Add(detailsWeather5);
+        }
+
+        public async Task Request()
+        {
+            openWeather = await httpOpenWeatherClient.GetWeatherByCityNameAsync(lblCity.Text);
+        }
+    }
+}
