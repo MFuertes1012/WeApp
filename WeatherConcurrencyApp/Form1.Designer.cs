5f5bb73b952b38fee1ecea7d8fb2fcb9d417afb9
Margie Fuertes <85895396+MFuertes1012@users.noreply.github.com>
2022-04-27 11:06:52 -0600

    Add files via upload

---
+++ WeatherConcurrencyApp/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace WeatherConcurrencyApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flpContent = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.cmbCity = new System.Windows.Forms.ComboBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flpContent
+            // 
+            this.flpContent.AutoScroll = true;
+            this.flpContent.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flpContent.Location = new System.Drawing.Point(0, 98);
+            this.flpContent.Margin = new System.Windows.Forms.Padding(4);
+            this.flpContent.Name = "flpContent";
+            this.flpContent.Size = new System.Drawing.Size(860, 564);
+            this.flpContent.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtCity);
+            this.panel1.Controls.Add(this.cmbCity);
+            this.panel1.Controls.Add(this.btnOk);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(860, 90);
+            this.panel1.TabIndex = 1;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(82, 26);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 22);
+            this.txtCity.TabIndex = 2;
+            // 
+            // cmbCity
+            // 
+            this.cmbCity.FormattingEnabled = true;
+            this.cmbCity.Location = new System.Drawing.Point(304, 26);
+            this.cmbCity.Name = "cmbCity";
+            this.cmbCity.Size = new System.Drawing.Size(182, 24);
+            this.cmbCity.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(661, 26);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(169, 46);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Aceptar";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 662);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flpContent);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmMain";
+            this.Text = "Weather Async App";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpContent;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOk;
+        public System.Windows.Forms.ComboBox cmbCity;
+        public System.Windows.Forms.TextBox txtCity;
+    }
+}
+
---
+++ WeatherConcurrencyApp/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeatherConcurrencyApp.Common;
+using WeatherConcurrencyApp.Infrastructure.OpenWeatherClient;
+using WeatherConcurrentApp.Domain.Entities;
+
+namespace WeatherConcurrencyApp
+{
+    public partial class FrmMain : Form
+    {
+        public HttpOpenWeatherClient httpOpenWeatherClient;
+        public OpenWeather openWeather;
+        public FrmMain()
+        {
+            httpOpenWeatherClient = new HttpOpenWeatherClient();
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Task.Run(Request).Wait();
+                if(openWeather == null)
+                {
+                    throw new NullReferenceException("Fallo al obtener el objeto OpeWeather.");
+                }
+                
+                WeatherPanel weatherPanel = new WeatherPanel();
+                weatherPanel.lblCity.Text = txtCity.Text;
+                weatherPanel.lblTemperature.Text = openWeather.Main.Temp.ToString() + "C";
+                weatherPanel.lblWeather.Text = openWeather.Weather[0].Main;
+                weatherPanel.picIcon.ImageLocation = $"{AppSettings.ApiIcon}" + openWeather.Weather[0].Icon + ".png";
+                flpContent.Controls.Add(weatherPanel);
+                
+            }
+            catch (Exception)
+            {
+                
+            }
+           
+        }
+
+        public async Task Request()
+        {
+            //txtCity es temporal para mientras hago lo otro
+           openWeather = await httpOpenWeatherClient.GetWeatherByCityNameAsync(txtCity.Text);
+        }
+    }
+}
